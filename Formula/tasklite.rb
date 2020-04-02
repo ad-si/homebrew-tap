555f1b8d5158960ed31b538fe8c0d316cf8021a0
Adrian Sieber <mail@adriansieber.com>
2020-04-02 23:51:25 +0000

    Add Homebrew Formula for TaskLite

---
+++ Formula/tasklite.rb
@@ -0,0 +1,18 @@
+class Tasklite < Formula
+  desc "The CLI task manager for power users"
+  homepage "https://tasklite.org"
+  url "https://github.com/ad-si/TaskLite/releases/download/v0.3.0.0/tasklite_macos_x86_64.zip"
+  version "0.3.0.0"
+  sha256 "b77245e0bf438b6c38913c360cf2a72f61834d632f8c2ea2eefcb015c30e8dcb"
+
+  bottle :unneeded
+
+  def install
+    bin.install "tasklite"
+  end
+
+  test do
+    system "#{bin}/tasklite", "help"
+    system "#{bin}/tasklite", "add", "Buy milk"
+  end
+end
