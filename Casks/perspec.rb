b02733587ac0633342601b14c778cce25020bb7f
Adrian Sieber <mail@adriansieber.com>
2020-11-17 16:16:16 +0000

    Update to latest version of Perspec

--- Casks/perspec.rb
+++ Casks/perspec.rb
@@ -1,15 +1,13 @@
 cask 'perspec' do
-  version '0.1.2.0'
+  version '0.1.3.0'
   sha256 'aff4a9cc287281a898da3c326a5e6cd7a55ce7ae51982b9fba77a395d2213eef'
 
-  url "https://github.com/ad-si/Perspec/releases/download/v#{version}/Perspec.app.zip"
+  url "https://github.com/ad-si/Perspec/releases/download/v#{version}/Perspec_macOS_10-15.zip"
   appcast 'https://github.com/ad-si/Perspec/releases.atom'
   name 'Perspec'
   homepage 'https://github.com/ad-si/Perspec'
 
-  app 'Perspec.app'
+  depends_on macos: :catalina
 
-  caveats 'Due to a render bug of gloss, ' \
-    'the initial view is sometimes incorrectly scaled ' \
-    'and the window needs to be rescaled to force a redraw. '
+  app 'Perspec_macOS_10-15/Perspec.app'
 end
