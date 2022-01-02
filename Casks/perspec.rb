e2baa8cd30e5d1d712843410c86601a444036d07
Adrian Sieber <36796532+ad-si@users.noreply.github.com>
2022-01-02 16:54:07 +0000

    Upgrade to Perspec 0.2.0.0

--- Casks/perspec.rb
+++ Casks/perspec.rb
@@ -1,13 +1,13 @@
 cask 'perspec' do
-  version '0.1.3.0'
-  sha256 '865f3ed379210208a732eed3773f38289bb4b4f680025c9de99237f3b66c6a21'
+  version '0.2.0.0'
+  sha256 '036545b01d92651e65a356be5215fcc9b205a682c751f7c334361e867f0b1b12'
 
-  url "https://github.com/ad-si/Perspec/releases/download/v#{version}/Perspec_macOS_10-15.zip"
-  appcast 'https://github.com/ad-si/Perspec/releases.atom'
+  url "https://github.com/feramhq/Perspec/releases/download/v#{version}/perspec-app_macos-12.1_x86_64.zip"
+  appcast 'https://github.com/feramhq/Perspec/releases.atom'
   name 'Perspec'
-  homepage 'https://github.com/ad-si/Perspec'
+  homepage 'https://github.com/feramhq/Perspec'
 
-  depends_on macos: :catalina
+  depends_on macos: ">= :big_sur"
 
-  app 'Perspec_macOS_10-15/Perspec.app'
+  app 'Perspec.app'
 end
