955aff4049aad50c1d0f965e7b674e2bea48724c
Adrian Sieber <mail@adriansieber.com>
2026-05-12 19:38:31 +0000

    Point Perspec cask at ad-si/Perspec (repo moved from feramhq)

--- Casks/perspec.rb
+++ Casks/perspec.rb
@@ -1,11 +1,11 @@
-cask 'perspec' do
-  version '0.2.0.0'
-  sha256 '036545b01d92651e65a356be5215fcc9b205a682c751f7c334361e867f0b1b12'
+cask "perspec" do
+  version "0.2.0.0"
+  sha256 "036545b01d92651e65a356be5215fcc9b205a682c751f7c334361e867f0b1b12"
 
-  url "https://github.com/feramhq/Perspec/releases/download/v#{version}/perspec-app_macos-12.1_x86_64.zip"
+  url "https://github.com/ad-si/Perspec/releases/download/v#{version}/perspec-app_macos-12.1_x86_64.zip"
   name "Perspec"
   desc "Scriptable desktop app to correct the perspective of images"
-  homepage "https://github.com/feramhq/Perspec"
+  homepage "https://github.com/ad-si/Perspec"
 
   livecheck do
     url :url
