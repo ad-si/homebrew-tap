b4b0de2525eea27e0a376268afafaabc47eb2985
Adrian Sieber <36796532+ad-si@users.noreply.github.com>
2026-07-09 21:23:51 +0000

    perspec: update to 1.0.0.0 (universal arm64 + x86_64)

--- Casks/perspec.rb
+++ Casks/perspec.rb
@@ -1,8 +1,15 @@
 cask "perspec" do
-  version "0.2.0.0"
-  sha256 "036545b01d92651e65a356be5215fcc9b205a682c751f7c334361e867f0b1b12"
+  version "1.0.0.0"
 
-  url "https://github.com/ad-si/Perspec/releases/download/v#{version}/perspec-app_macos-12.1_x86_64.zip"
+  on_arm do
+    sha256 "d85f5da7d8815aab970e60455ee5f6acb0228c5659182b54392fde922d186af2"
+    url "https://github.com/ad-si/Perspec/releases/download/v#{version}/perspec-app_macos-14_arm64.zip"
+  end
+  on_intel do
+    sha256 "fffcca5d0f24db58cf0d09f7803b2e88ecf705a88af86655336d3a4bb017bc5a"
+    url "https://github.com/ad-si/Perspec/releases/download/v#{version}/perspec-app_macos-15-intel_x86_64.zip"
+  end
+
   name "Perspec"
   desc "Scriptable desktop app to correct the perspective of images"
   homepage "https://github.com/ad-si/Perspec"
@@ -13,7 +20,6 @@ cask "perspec" do
   end
 
   depends_on macos: ">= :big_sur"
-  depends_on arch: :x86_64
 
   app "Perspec.app"
 end
